54edc8238dd1757f8123de508bea688b62f0a963
浜田　俊祐 <hamashun0820@icloud.com>
2024-03-30 07:01:26 +0900

    add: input

---
+++ swaying-memory/input.go
@@ -0,0 +1,29 @@
+package swayingmemory
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+// Input manages the input state including gamepads and keyboards.
+type Input struct {
+	gamepadIDs []ebiten.GamepadID
+}
+
+// GamepadIDButtonPressed returns a gamepad ID where at least one button is pressed.
+// If no button is pressed, GamepadIDButtonPressed returns -1.
+func (i *Input) GamepadIDButtonPressed() ebiten.GamepadID {
+	i.gamepadIDs = ebiten.AppendGamepadIDs(i.gamepadIDs[:0])
+	for _, id := range i.gamepadIDs {
+		for b := ebiten.GamepadButton(0); b <= ebiten.GamepadButtonMax; b++ {
+			if ebiten.IsGamepadButtonPressed(id, b) {
+				return id
+			}
+		}
+	}
+	return -1
+}
+
+func (i *Input) IsClicked() bool {
+	return inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft)
+}

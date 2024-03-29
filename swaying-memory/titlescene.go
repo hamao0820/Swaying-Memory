8433eb08c195e4f9f2c6c04a7982164c9388fa71
浜田　俊祐 <hamashun0820@icloud.com>
2024-03-30 07:51:29 +0900

    feat: subtitle

--- swaying-memory/titlescene.go
+++ swaying-memory/titlescene.go
@@ -16,4 +16,7 @@ func (s *TitleScene) Update(state *GameState) error {
 func (s *TitleScene) Draw(screen *ebiten.Image) {
 	title := "Swaying Memory"
 	drawBigText(screen, title, ScreenWidth/2-getBigTextWidth(title)/2, 100, color.White)
+
+	subtitle := "Press Enter to start"
+	drawNormalText(screen, subtitle, ScreenWidth/2-getNormalTextWidth(subtitle)/2, 200, color.White)
 }

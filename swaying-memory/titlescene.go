bb494fff66c3b6b4e23e2996527e947acee9cb73
浜田　俊祐 <hamashun0820@icloud.com>
2024-03-30 10:11:38 +0900

    fix: message

--- swaying-memory/titlescene.go
+++ swaying-memory/titlescene.go
@@ -70,6 +70,6 @@ func (s *TitleScene) Draw(screen *ebiten.Image) {
 	title := "Swaying Memory"
 	drawBigText(screen, title, ScreenWidth/2-getBigTextWidth(title)/2, 100, color.White)
 
-	subtitle := "Press Enter to start"
+	subtitle := "Press Space to start"
 	drawNormalText(screen, subtitle, ScreenWidth/2-getNormalTextWidth(subtitle)/2, 200, color.White)
 }
